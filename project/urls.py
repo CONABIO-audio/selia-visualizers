8ac2db4ab6afb726241c8b175a88dbf03d303ac3
mbsantiago <santiago.mbal@gmail.com>
2020-09-23 15:13:57 -0500

    add admin to project

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
 
 
 urlpatterns = [
+    url('admin/', admin.site.urls),
     url(
         r'^visualizers/',
         include(('selia_visualizers.urls', 'selia_visualizers'))),
