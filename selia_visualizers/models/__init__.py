ae430e25d46e164de8394329b01f309d15299415
mbsantiago <santiago.mbal@gmail.com>
2020-09-25 17:07:52 -0500

    moved visualizer model to this module

---
+++ selia_visualizers/models/__init__.py
@@ -0,0 +1,12 @@
+from .visualizer_component_item_type import VisualizerComponentItemType
+from .visualizer_component import visualizer_path
+from .visualizer_component import VisualizerComponent
+from .visualizer import Visualizer
+
+
+__all__ = [
+    'VisualizerComponentItemType',
+    'visualizer_path',
+    'VisualizerComponent',
+    'Visualizer',
+]
